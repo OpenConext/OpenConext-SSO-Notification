cdb1d0e5638d6c1ca3f3ec10706475ebfc7fa51f
Thomas Beekman <t.beekman@kennisnet.nl>
2023-04-17 13:21:14 +0200

    ENT-4637: Implement confederational SSO notification

---
+++ oc-sso-notificatie/src/main/java/nl/kennisnet/services/web/exception/NoMatchFoundException.java
@@ -0,0 +1,10 @@
+package nl.kennisnet.services.web.exception;
+
+/**
+ * @author T. Beekman (Beekman.Thomas@kpmg.nl)
+ */
+public class NoMatchFoundException extends Exception {
+
+    private static final long serialVersionUID = -3011165738203242932L;
+
+}
